b57840adb016ecd51154efcddb184314ef96b3e9
developerOlive <chloehiho@gmail.com>
2023-08-16 11:41:39 +0900

    feat: 초기 세팅

---
+++ src/main/java/com/chartcalculator/ChartCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.chartcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChartCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chartcalculator/ChartCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chartcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
